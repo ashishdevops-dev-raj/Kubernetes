7c10320d943a328429582f581bffeaa687a6658c
Ashish Raj <ashishrajdevops@gmail.com>
2025-07-19 13:51:14 +0530

    Create prepare.sh

---
+++ Kubernetes-Cluster-Setup/prepare.sh
@@ -0,0 +1,20 @@
+sudo apt update && sudo apt upgrade -y
+
+# Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+# Load kernel modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+sudo modprobe br_netfilter
+
+# Set sysctl params for Kubernetes networking
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
