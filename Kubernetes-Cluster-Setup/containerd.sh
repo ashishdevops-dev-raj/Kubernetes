3c22f53b4078f82f6a3626c8051d507e94edd5f5
Ashish Raj <ashishrajdevops@gmail.com>
2025-07-19 13:51:44 +0530

    Create containerd.sh

---
+++ Kubernetes-Cluster-Setup/containerd.sh
@@ -0,0 +1,11 @@
+sudo apt install -y containerd
+
+# Generate default config and update cgroup driver
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
+# Restart and enable containerd
+sudo systemctl restart containerd
+sudo systemctl enable containerd
